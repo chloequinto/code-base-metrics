9153c2c4262e58214cac0e626cd095701de852f4
chloequinto <cquinto@stevens.edu>
2018-08-31 08:34:45 -0400

    t-tests on CCN, nloc, tokens

--- clean_django_rails.R
+++ clean_django_rails.R
@@ -1,5 +1,6 @@
 library(xlsx)
 library(XLConnect)
+library(MASS)
 
 #if you reach the GC overhead limit 
 options(java.parameters = "-Xmx1024m")
@@ -9,7 +10,15 @@ options(java.parameters = "-Xmx8000m")
 library(devtools)
 
 install_version("XLConnectJars", version = "0.2-12", repos = "http://cran.us.r-project.org") 
-install_version("XLConnect", version = "0.2-12", repos = "http://cran.us.r-project.org")`
+#install_version("XLConnect", version = "0.2-12", repos = "http://cran.us.r-project.org")`
+
+#Files and their respective release year 
+#2011 - d.1.3 and r.3.0
+#2012 - d.1.4 and r.3.2
+#2013 - d.1.6 and r.4.0 
+#2014 - d.1.7 and r.4.1 
+#2015 - d.1.9 and r.4.2 
+#2016 - d.1.10 and r.5.0 
 
 
 #Read and Write the data files 
@@ -93,4 +102,127 @@ m_read_r_4_1 <- mean(read_r_4_1$V2, na.rm = FALSE)
 m_read_r_4_2 <- mean(read_r_4_2$V2, na.rm = FALSE)
 m_read_r_5_0 <- mean(read_r_5_0$V2, na.rm = FALSE)
 
+#Calculate the T-Tests for Unequal Variances - CCN
 
+#2011 Calculations
+d.2011_2 <- read_django_1.3$V2
+r.2011_2 <- read_r_3_1$V2
+calculations_2011_2<-t.test(d.2011_2,r.2011_2,var.equal = FALSE)
+write.table(data.frame(unlist(calculations_2011_2)), append = TRUE, file="C:/Users/Chloe Quinto/Desktop/Cleaned up files/CCN/all_ccn.tsv", sep="\t", col.names = T, row.names = T)
+
+
+#2012 Calculations
+d.2012_2 <- read_django_1.4$V2
+r.2012_2 <- read_r_3_2$V2
+calculations_2012_2 <- t.test(d.2012_2, r.2012_2, var.equal = FALSE)
+write.table(data.frame(unlist(calculations_2012_2)), append = TRUE, file="C:/Users/Chloe Quinto/Desktop/Cleaned up files/CCN/all_ccn.tsv", sep="\t", col.names = T, row.names = T)
+
+#2013 Calculations
+d.2013_2 <- read_django_1.6$V2
+r.2013_2 <- read_r_4_0$V2
+calculations_2013_2<-t.test(d.2013_2,r.2013_2,var.equal = FALSE)
+write.table(data.frame(unlist(calculations_2013_2)), append = TRUE, file="C:/Users/Chloe Quinto/Desktop/Cleaned up files/CCN/all_ccn.tsv", sep="\t", col.names = T, row.names = T)
+
+
+#2014 Calculations
+d.2014_2 <- read_django_1.7$V2
+r.2014_2 <- read_r_4_1$V2
+calculations_2014_2 <- t.test(d.2014_2, r.2014_2, var.equal = FALSE)
+write.table(data.frame(unlist(calculations_2014_2)), append = TRUE, file="C:/Users/Chloe Quinto/Desktop/Cleaned up files/CCN/all_ccn.tsv", sep="\t", col.names = T, row.names = T)
+
+#2015 Calculations
+d.2015_2 <- read_django_1.9$V2
+r.2015_2 <- read_r_4_2$V2
+calculations_2015_2<-t.test(d.2015_2,r.2015_2,var.equal = FALSE)
+write.table(data.frame(unlist(calculations_2015_2)), append = TRUE, file="C:/Users/Chloe Quinto/Desktop/Cleaned up files/NLOC/all_nloc.tsv", sep="\t", col.names = T, row.names = T)
+
+
+#2016 Calculations
+d.2016_2 <- read_django_1.10$V2
+r.2016_2 <- read_r_5_0$V2
+calculations_2016_2 <- t.test(d.2016_2, r.2016_2, var.equal = FALSE)
+write.table(data.frame(unlist(calculations_2016_2)), append = TRUE, file="C:/Users/Chloe Quinto/Desktop/Cleaned up files/NLOC/all_nloc.tsv", sep="\t", col.names = T, row.names = T)
+
+
+#Calculate the T-Tests for Unequal Variances - NLOC 
+
+#2011 Calculations
+d.2011_1 <- read_django_1.3$V1
+r.2011_1 <- read_r_3_1$V1
+calculations_2011_1<-t.test(d.2011_1,r.2011_1,var.equal = FALSE)
+write.table(data.frame(unlist(calculations_2011_1)), append = TRUE, file="C:/Users/Chloe Quinto/Desktop/Cleaned up files/NLOC/all_nloc.tsv", sep="\t", col.names = T, row.names = T)
+
+
+#2012 Calculations
+d.2012_1 <- read_django_1.4$V1
+r.2012_1 <- read_r_3_2$V1
+calculations_2012_1 <- t.test(d.2012_1, r.2012_1, var.equal = FALSE)
+write.table(data.frame(unlist(calculations_2012_1)), append = TRUE, file="C:/Users/Chloe Quinto/Desktop/Cleaned up files/NLOC/all_nloc.tsv", sep="\t", col.names = T, row.names = T)
+
+#2013 Calculations
+d.2013_1 <- read_django_1.6$V1
+r.2013_1 <- read_r_4_0$V1
+calculations_2013_1<-t.test(d.2013_1,r.2013_1,var.equal = FALSE)
+write.table(data.frame(unlist(calculations_2013_1)), append = TRUE, file="C:/Users/Chloe Quinto/Desktop/Cleaned up files/NLOC/all_nloc.tsv", sep="\t", col.names = T, row.names = T)
+
+
+#2014 Calculations
+d.2014_1 <- read_django_1.7$V1
+r.2014_1 <- read_r_4_1$V1
+calculations_2014_1 <- t.test(d.2014_1, r.2014_1, var.equal = FALSE)
+write.table(data.frame(unlist(calculations_2014_1)), append = TRUE, file="C:/Users/Chloe Quinto/Desktop/Cleaned up files/NLOC/all_nloc.tsv", sep="\t", col.names = T, row.names = T)
+
+#2015 Calculations
+d.2015_1 <- read_django_1.9$V1
+r.2015_1 <- read_r_4_2$V1
+calculations_2015_1<-t.test(d.2015_1,r.2015_1,var.equal = FALSE)
+write.table(data.frame(unlist(calculations_2015_1)), append = TRUE, file="C:/Users/Chloe Quinto/Desktop/Cleaned up files/NLOC/all_nloc.tsv", sep="\t", col.names = T, row.names = T)
+
+
+#2016 Calculations
+d.2016_1 <- read_django_1.10$V1
+r.2016_1 <- read_r_5_0$V1
+calculations_2016_1 <- t.test(d.2016_1, r.2016_1, var.equal = FALSE)
+write.table(data.frame(unlist(calculations_2016_1)), append = TRUE, file="C:/Users/Chloe Quinto/Desktop/Cleaned up files/NLOC/all_nloc.tsv", sep="\t", col.names = T, row.names = T)
+
+
+#Calculate the T-Tests for Unequal Variances - TOKEN
+
+#2011 Calculations
+d.2011_3 <- read_django_1.3$V3
+r.2011_3 <- read_r_3_1$V3
+calculations_2011_3<-t.test(d.2011_3,r.2011_3,var.equal = FALSE)
+write.table(data.frame(unlist(calculations_2011_3)), append = TRUE, file="C:/Users/Chloe Quinto/Desktop/Cleaned up files/TOKEN/all_token.tsv", sep="\t", col.names = T, row.names = T)
+
+
+#2012 Calculations
+d.2012_3 <- read_django_1.4$V3
+r.2012_3 <- read_r_3_2$V3
+calculations_2012_3 <- t.test(d.2012_3, r.2012_3, var.equal = FALSE)
+write.table(data.frame(unlist(calculations_2012_3)), append = TRUE, file="C:/Users/Chloe Quinto/Desktop/Cleaned up files/TOKEN/all_token.tsv", sep="\t", col.names = T, row.names = T)
+
+#2013 Calculations
+d.2013_3 <- read_django_1.6$V3
+r.2013_3 <- read_r_4_0$V3
+calculations_2013_3<-t.test(d.2013_3,r.2013_3,var.equal = FALSE)
+write.table(data.frame(unlist(calculations_2013_3)), append = TRUE, file="C:/Users/Chloe Quinto/Desktop/Cleaned up files/TOKEN/all_token.tsv", sep="\t", col.names = T, row.names = T)
+
+
+#2014 Calculations
+d.2014_3 <- read_django_1.7$V3
+r.2014_3 <- read_r_4_1$V3
+calculations_2014_3 <- t.test(d.2014_3, r.2014_3, var.equal = FALSE)
+write.table(data.frame(unlist(calculations_2014_3)), append = TRUE, file="C:/Users/Chloe Quinto/Desktop/Cleaned up files/TOKEN/all_token.tsv", sep="\t", col.names = T, row.names = T)
+
+#2015 Calculations
+d.2015_3 <- read_django_1.9$V3
+r.2015_3 <- read_r_4_2$V3
+calculations_2015_3<-t.test(d.2015_3,r.2015_3,var.equal = FALSE)
+write.table(data.frame(unlist(calculations_2015_3)), append = TRUE, file="C:/Users/Chloe Quinto/Desktop/Cleaned up files/TOKEN/all_token.tsv", sep="\t", col.names = T, row.names = T)
+
+
+#2016 Calculations
+d.2016_3 <- read_django_1.10$V3
+r.2016_3 <- read_r_5_0$V3
+calculations_2016_3 <- t.test(d.2016_3, r.2016_3, var.equal = FALSE)
+write.table(data.frame(unlist(calculations_2016_3)), append = TRUE, file="C:/Users/Chloe Quinto/Desktop/Cleaned up files/TOKEN/all_token.tsv", sep="\t", col.names = T, row.names = T)
