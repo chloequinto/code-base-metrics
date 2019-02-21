8f4de98530c49cf7545f6b4f806fd127177f9b9c
chloequinto <cquinto@stevens.edu>
2019-02-21 00:11:39 -0500

    warnings are on

--- t_tests.R
+++ t_tests.R
@@ -24,9 +24,20 @@ r.5.0 <- read.csv("rails_5.0.csv", header = FALSE)
 ############### Warnings Count Only ###################
 setwd("/Users/chloequinto/Documents/code-base-metrics/input results/warnings_cleaned")
 d.1.3.w <- read.csv("d.1.3.w.csv", header = FALSE)
+d.1.4.w <- read.csv("d.1.4.w.csv", header = FALSE)
+d.1.6.w <- read.csv("d.1.6.w.csv", header = FALSE)
+d.1.7.w <- read.csv("d.1.7.w.csv", header = FALSE)
+d.1.9.w <- read.csv("d.1.9.w.csv", header = FALSE)
+d.1.10.w <- read.csv("d.1.10.w.csv", header = FALSE)
 
 
 r.3.1.w <- read.csv("r.3.1.w.csv", header = FALSE)
+r.3.2.w <- read.csv("r.3.2.w.csv", header = FALSE)
+r.4.0.w <- read.csv("r.4.0.w.csv", header = FALSE)
+r.4.1.w <- read.csv("r.4.1.w.csv", header = FALSE)
+r.4.2.w <- read.csv("r.4.2.w.csv", header = FALSE)
+r.5.0.w <- read.csv("r.5.0.w.csv", header = FALSE)
+
 
 
 #V1 -> LOC
@@ -41,7 +52,7 @@ r.3.1.w <- read.csv("r.3.1.w.csv", header = FALSE)
 #V10 -> ??
 #V11 -> ??
 
-head(d.1.3)
+
 
 ############ CALCULATIONS ############## 
 # 2011 Calculations 
@@ -52,38 +63,38 @@ write.table(data.frame(unlist(WRN_2011)), append = TRUE, file="/Users/chloequint
 
 
 # 2012 Calculations 
-d.2012_FUNC <- d.1.4$V5
-r.2012_FUNC <- r.3.2$V5
-FUNC_2012<-t.test(d.2012_FUNC,r.2012_FUNC,var.equal = FALSE)
-write.table(data.frame(unlist(FUNC_2012)), append = TRUE, file="/Users/chloequinto/Documents/code-base-metrics/Results/FUNC_2012.txt", sep="\t", col.names = T, row.names = T)
+d.2012_WRN <- d.1.4.w$V2
+r.2012_WRN <- r.3.2.w$V2
+WRN_2012<-t.test(d.2012_WRN ,r.2012_WRN,var.equal = FALSE)
+write.table(data.frame(unlist(WRN_2012)), append = TRUE, file="/Users/chloequinto/Documents/code-base-metrics/Results/WRN_2012.txt", sep="\t", col.names = T, row.names = T)
 
 
 # 2013 Calculations 
-d.2013_FUNC <- d.1.6$V5
-r.2013_FUNC <- r.4.0$V5
-FUNC_2013<-t.test(d.2013_FUNC,r.2013_FUNC ,var.equal = FALSE)
-write.table(data.frame(unlist(FUNC_2013)), append = TRUE, file="/Users/chloequinto/Documents/code-base-metrics/Results/FUNC_2013.txt", sep="\t", col.names = T, row.names = T)
+d.2013_WRN <- d.1.6.w$V2
+r.2013_WRN <- r.4.0.w$V2
+WRM_2013<-t.test(d.2013_WRN ,r.2013_WRN ,var.equal = FALSE)
+write.table(data.frame(unlist(WRM_2013)), append = TRUE, file="/Users/chloequinto/Documents/code-base-metrics/Results/WRN_2013.txt", sep="\t", col.names = T, row.names = T)
 
 
 #2014 Calculations
-d.2014_FUNC<- d.1.7$V5
-r.2014_FUNC <- r.4.1$V5
-FUNC_2014 <- t.test(d.2014_FUNC, r.2014_FUNC, var.equal = FALSE)
-write.table(data.frame(unlist(FUNC_2014)), append = TRUE, file="/Users/chloequinto/Documents/code-base-metrics/Results/FUNC_2014.txt", sep="\t", col.names = T, row.names = T)
+d.2014_WRN <- d.1.7.w$V2
+r.2014_WRN <- r.4.1.w$V2
+WRN_2014 <- t.test(d.2014_WRN, r.2014_WRN, var.equal = FALSE)
+write.table(data.frame(unlist(WRN_2014)), append = TRUE, file="/Users/chloequinto/Documents/code-base-metrics/Results/WRN_2014.txt", sep="\t", col.names = T, row.names = T)
 
 
 
 #2015 Calculations
-d.2015_NLOC<- d.1.9$V2
-r.2015_NLOC <- r.4.2$V2
-NLOC_2015 <- t.test(d.2015_NLOC,r.2015_NLOC , var.equal = FALSE)
-write.table(data.frame(unlist(NLOC_2015 )), append = TRUE, file="/Users/chloequinto/Documents/code-base-metrics/Results/NLOC_2015.txt", sep="\t", col.names = T, row.names = T)
+d.2015_WRN<- d.1.9.w$V2
+r.2015_WRN <- r.4.2.w$V2
+WRN_2015 <- t.test(d.2014_WRN,r.2015_WRN , var.equal = FALSE)
+write.table(data.frame(unlist(WRN_2015)), append = TRUE, file="/Users/chloequinto/Documents/code-base-metrics/Results/WRN_2015.txt", sep="\t", col.names = T, row.names = T)
 
 
 #2016 Calculations 
-d.2016_FUNC<- d.1.10$V5
-r.2016_FUNC <- r.5.0$V5
-FUNC_2016 <- t.test(d.2016_FUNC,r.2016_FUNC, var.equal = FALSE)
-write.table(data.frame(unlist(FUNC_2016)), append = TRUE, file="/Users/chloequinto/Documents/code-base-metrics/Results/FUNC_2016.txt", sep="\t", col.names = T, row.names = T)
+d.2016_WRN<- d.1.10.w$V2
+r.2016_WRN <- r.5.0.w$V2
+WRN_2016 <- t.test(d.2016_WRN,r.2016_WRN, var.equal = FALSE)
+write.table(data.frame(unlist(WRN_2016 )), append = TRUE, file="/Users/chloequinto/Documents/code-base-metrics/Results/WRN_2016.txt", sep="\t", col.names = T, row.names = T)
 
 
